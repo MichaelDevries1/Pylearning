a7f833a0b7680dd4ca123959976757de1ccdbc09
Michael DeVries <mdevrie4@msudenver.edu>
2019-04-30 14:05:02 -0600

    Made an entry field

--- WeatherPractice.py
+++ WeatherPractice.py
@@ -7,7 +7,12 @@ root = tk.Tk()
 
 canvas = tk.Canvas(root, height=WHEIGHT, width=WWIDTH).pack()
 
-textFrame = tk.Frame(canvas, bg='blue').place(relx=.2, rely=.2, relwidth=.5, height=20)
-cityEntry = tk.Entry(textFrame, bg='white').place(relwidth=.95, relheight=.95)
+textFrame = tk.Frame(root, bg='blue')
+textEntry = tk.Entry(textFrame, font=12)
+acceptance = tk.Button(textFrame, text='Find')
+
+textFrame.place(relx=.5, rely=.1, relwidth=.75, relheight=.05, anchor='n')
+textEntry.place(relwidth=.65, relheight=1)
+acceptance.place(relx=.65, relwidth=.35, relheight=1)
 
 root.mainloop()
