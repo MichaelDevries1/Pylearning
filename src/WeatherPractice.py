58011dceae745ff2687158717e4bed806d8a6077
Michael DeVries <mdevrie4@msudenver.edu>
2019-05-08 17:06:27 -0600

    prints 5 day weather info into the console

--- src/WeatherPractice.py
+++ src/WeatherPractice.py
@@ -4,17 +4,19 @@ import requests
 
 FONT = 30
 IMAGE_NAME = 'landscape.jpeg'
-
-# api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
-# ca2b89c6af0d2220dd8ab6b4dfcdd439
+# Current weather: api.openweathermap.org/data/2.5/weather?q={city name}
+# 5 day weather: api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+# API key: ca2b89c6af0d2220dd8ab6b4dfcdd439
 
 def city_entry(entry):
     print("Text entered:", entry)
 
 def get_weather(city):
     weather_api_key = 'ca2b89c6af0d2220dd8ab6b4dfcdd439'
-    url = 'api.openweathermap.org/data/2.5/forecast'
+    url = 'http://api.openweathermap.org/data/2.5/forecast'
     params = {'APPID': weather_api_key, 'q': city, 'units': 'imperial'}
+    response = requests.get(url, params=params)
+    print(response.json())
 
 root = tk.Tk()
 root.title('Weather App')
@@ -30,7 +32,7 @@ bg_label.place(relwidth=1, relheight=1)
 # City find box
 input_frame = tk.Frame(root, bd=5, bg='#80c1ff')
 city_input = tk.Entry(input_frame, font=FONT)
-find_button = tk.Button(input_frame, text='Find', font=FONT, command=lambda: city_entry(city_input.get()))
+find_button = tk.Button(input_frame, text='Find', font=FONT, command=lambda: get_weather(city_input.get()))
 
 input_frame.place(anchor='n', relx=.5, rely=.1, relwidth=.75, relheight=.07)
 city_input.place(relwidth=.65, relheight=1)
