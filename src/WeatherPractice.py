7097af4818dcdf30ece85252b5fa8438c940b5d9
Michael DeVries <mdevrie4@msudenver.edu>
2019-05-08 16:39:30 -0600

    started pulling the weather api in

--- src/WeatherPractice.py
+++ src/WeatherPractice.py
@@ -1,13 +1,20 @@
 import tkinter as tk
 from PIL import ImageTk, Image
+import requests
 
 FONT = 30
 IMAGE_NAME = 'landscape.jpeg'
 
+# api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+# ca2b89c6af0d2220dd8ab6b4dfcdd439
 
-def button_click():
-    print("Button Clicked!!!")
+def city_entry(entry):
+    print("Text entered:", entry)
 
+def get_weather(city):
+    weather_api_key = 'ca2b89c6af0d2220dd8ab6b4dfcdd439'
+    url = 'api.openweathermap.org/data/2.5/forecast'
+    params = {'APPID': weather_api_key, 'q': city, 'units': 'imperial'}
 
 root = tk.Tk()
 root.title('Weather App')
@@ -23,7 +30,7 @@ bg_label.place(relwidth=1, relheight=1)
 # City find box
 input_frame = tk.Frame(root, bd=5, bg='#80c1ff')
 city_input = tk.Entry(input_frame, font=FONT)
-find_button = tk.Button(input_frame, text='Find', font=FONT, command=button_click)
+find_button = tk.Button(input_frame, text='Find', font=FONT, command=lambda: city_entry(city_input.get()))
 
 input_frame.place(anchor='n', relx=.5, rely=.1, relwidth=.75, relheight=.07)
 city_input.place(relwidth=.65, relheight=1)
