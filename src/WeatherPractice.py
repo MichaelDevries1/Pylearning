a2959de90e0098de44313a88e32d64aaecfb5bf8
Michael DeVries <mdevrie4@msudenver.edu>
2019-05-08 22:02:27 -0600

    Got the weather information to display correctly

--- src/WeatherPractice.py
+++ src/WeatherPractice.py
@@ -1,15 +1,19 @@
 import tkinter as tk
+from tkinter import font
 from PIL import ImageTk, Image
 import requests
 
 FONT = 30
 IMAGE_NAME = 'landscape.jpeg'
+
 # Current weather: api.openweathermap.org/data/2.5/weather?q={city name}
 # 5 day weather: api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
 # API key: ca2b89c6af0d2220dd8ab6b4dfcdd439
 
+
 def city_entry(entry):
     print("Text entered:", entry)
+
 
 def get_weather(city):
     weather_api_key = 'ca2b89c6af0d2220dd8ab6b4dfcdd439'
@@ -18,7 +22,20 @@ def get_weather(city):
     response = requests.get(url, params=params)
     weather = response.json()
 
-    print(weather['name'])
+    result_label['text'] = format(weather)
+
+
+def format(weather):
+    try:
+        name = weather['name']
+        description = weather['weather'][0]['description']
+        temp = weather['main']['temp']
+
+        final_string = 'City: %s\nCondition: %s\nTemp (°F): %s°' %(name, description, str(temp))
+    except:
+        final_string = 'We are unable to retrieve this information.'
+
+    return final_string
 
 root = tk.Tk()
 root.title('Weather App')
@@ -42,7 +59,7 @@ find_button.place(relx=.7, relwidth=.3, relheight=1)
 
 # Results box
 lower_frame = tk.Frame(root, bd=5, bg='#80c1ff')
-result_label = tk.Label(lower_frame)
+result_label = tk.Label(lower_frame, font=('Arial', 15), anchor='nw', justify='left', bg='white')
 
 lower_frame.place(anchor='n', relx=.5, rely=.22, relwidth=.75, relheight=.7)
 result_label.place(relwidth=1, relheight=1)
